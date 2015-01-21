a6dc9fb92673eff36317c92412e921818ba7673b
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-01-21 18:58:19 -0500

    asdf
    asdf

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-prompt("Hello World");
+var user = prompt("Hello, I would like to meet you. What is your name?").toUpperCase();
+
+	switch(user) {
+		case'Taewoo':
+		console.log("Hello Trumpet Buddy.").toUpperCase();
+		break;
+		case'Alex':
+		console.log("Can you ask me some sports questions?").toUpperCase();
+		break;
+		case'Jack':
+		console.log("There is two of you")
+		default:
+		console.log("I do not understand your choice. Please restart the webpage and start again.");
+}
