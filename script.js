f86be572d58b195ee88f1bac9ddf3b56bb35218f
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-01-21 19:04:27 -0500

    asdf
    asdf

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var user = prompt("Hello, I would like to meet you. What is your name?").toUpperCase();
 
 	switch(user) {
+		case'Amit':
+		var Amit = console.log("That's me!")
 		case'Taewoo':
-		var Taewoo = console.log("Hello Trumpet Buddy.").toUpperCase();
+		var Taewoo = console.log("Hello Trumpet Buddy.")
 		break;
 		case'Alex':
-		var Alex = console.log("Can you ask me some sports questions?").toUpperCase();
+		var Alex = console.log("Can you ask me some sports questions?")
 		break;
 		case'Jack':
 		var Jack = console.log("There is two of you!")
