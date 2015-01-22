8255e8805e127a11dbd491ffcbf317b2677d5c89
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-01-21 19:15:35 -0500

    asdf
    asdf

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ var user = prompt("Hello, I would like to meet you. What is your name?").toUpper
 
 	switch(user) {
 		case'Amit':
-		var Amit = console.log("That's me!")
+		var Amit = prompt("You have entered Amit, is that correct?").toUpperCase();
+		if("Yes") {
+			console.log("Hey, that's me!?!?");
+		}
+		else {
+			console.log("You must have written the wrong name then. Restart this website and try again.");
+		}
+		break;
 		case'Taewoo':
 		var Taewoo = console.log("Hello Trumpet Buddy.")
 		break;
@@ -11,4 +18,6 @@ var user = prompt("Hello, I would like to meet you. What is your name?").toUpper
 		break;
 		case'Jack':
 		var Jack = console.log("There is two of you!")
+		default:
+		console.log("I do not understand your choice. Please restart the webpage and start again.");
 }
