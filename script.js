dc647e9ab266bb27014cd782ecb09113a8f8dae1
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-01-21 20:39:58 -0500

    asdf
    asdf

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var user = prompt("Hello, I would like to meet you. What is your name?").toUpper
 		case'AMIT':
 		var amit = prompt("You have entered Amit, is that correct? (YES or NO)").toUpperCase();
 		if(amit === 'YES') {
-			console.log("Hey, that's me!?!?");
+			confirm("Hey, that's me!?!?");
 		}
 		else {
-			console.log("You must have written the wrong name then. Restart this website and try again.");
+			confirm("You must have written the wrong name then. Restart this website and try again.");
 		}
 		break;
 		case'NIKHIL':
@@ -16,36 +16,36 @@ var user = prompt("Hello, I would like to meet you. What is your name?").toUpper
 			confirm("Hi Nikhil");
 		}
 		else {
-			console.log("You must have written the wrong name then. Restart this website and try again.");
+			confirm("You must have written the wrong name then. Restart this website and try again.");
 		}
 		break;
 		case'TAEWOO':
 		var taewoo = prompt("You have entered Taewoo, is that correct? (YES or NO)").toUpperCase();
 		if(taewoo === 'YES') {
-		    console.log("Hello Trumpet Buddy");
+		    confirm("Hello Trumpet Buddy");
 		}
 		else {
-		    console.log("You must have written the wrong name then. Restart this website and try again.");
+		    confirm("You must have written the wrong name then. Restart this website and try again.");
 		}
 		break;
 		case'ALEX':
 		var alex = prompt("You have entered Alex, is that correct? (YES or NO)").toUpperCase();
 		if(alex === 'YES') {
-		    console.log("Can you ask me some sports questions?");
+		    confirm("Can you ask me some sports questions?");
 		}
 		else {
-		    console.log("You must have entered the wrong name then. Restart this website and try again");
+		    confirm("You must have entered the wrong name then. Restart this website and try again");
 		}
 		break;
 		case'JACK':
 		var jack = prompt("You have entered Jack, is that correct? (YES or NO)").toUpperCase();
 		if(jack === 'YES') {
-		    console.log("There is two of you! I don't know which one is asking!");
+		    confirm("There is two of you! I don't know which one is asking!");
 		}
 		else {
-		    console.log("You must have entered the wrong name. Restart this website and try again");
+		    confirm("You must have entered the wrong name. Restart this website and try again");
 		}
 		break;
 		default:
-		console.log("I do not understand your choice. Please restart the webpage and start again.");
+			confirm("I do not understand your choice. Please restart the webpage and start again.");
 }
