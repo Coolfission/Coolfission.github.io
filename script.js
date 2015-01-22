5a60c64517131b0171b8256f8f10782e77f8905e
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-01-21 19:42:14 -0500

    asdf
    asdf

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 var user = prompt("Hello, I would like to meet you. What is your name?").toUpperCase();
 
 	switch(user) {
-		case'Amit':
-		var Amit = prompt("You have entered Amit, is that correct?").toUpperCase();
-		if("Yes") {
+		case'AMIT':
+		var amit = prompt("You have entered Amit, is that correct? (YES or NO)").toUpperCase();
+		if(amit === 'YES') {
 			console.log("Hey, that's me!?!?");
 		}
 		else {
 			console.log("You must have written the wrong name then. Restart this website and try again.");
 		}
 		break;
-		case'Taewoo':
-		var Taewoo = console.log("Hello Trumpet Buddy.")
+		case'TAEWOO':
+		var taewoo = prompt("You have entered Taewoo, is that correct? (YES or NO)").toUpperCase();
+		if(taewoo === 'YES') {
+		    console.log("Hello Trumpet Buddy");
+		}
+		else {
+		    console.log("You must have written the wrong name then. Restart this website and try again.");
+		}
 		break;
-		case'Alex':
-		var Alex = console.log("Can you ask me some sports questions?")
+		case'ALEX':
+		var alex = prompt("You have entered Alex, is that correct? (YES or NO)").toUpperCase();
+		if(alex === 'YES') {
+		    console.log("Can you ask me some sports questions?");
+		}
+		else {
+		    console.log("You must have entered the wrong name then. Restart this website and try again");
+		}
 		break;
-		case'Jack':
-		var Jack = console.log("There is two of you!")
+		case'JACK':
+		var jack = prompt("You have entered Jack, is that correct? (YES or NO)").toUpperCase();
+		if(jack === 'YES') {
+		    console.log("There is two of you! I don't know which one is asking!");
+		}
+		else {
+		    console.log("You must have entered the wrong name. Restart this website and try again");
+		}
+		break;
 		default:
 		console.log("I do not understand your choice. Please restart the webpage and start again.");
 }
