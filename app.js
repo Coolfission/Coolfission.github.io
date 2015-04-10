ee4f56ace6d723fac52319392d1ef2246859f7ec
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-04-10 19:18:06 -0400

    asdf
    asdf

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //jQuery
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript'
 $(document).ready(function () {
 	$('div').click(function () {
 	$('div').fadeOut('fast');
