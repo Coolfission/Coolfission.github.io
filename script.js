e8d86f3fd53d655e60c1c4c808fe2e0c87ce8a54
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-05-26 21:28:45 -0400

    asdf
    asdf

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-/*Javascipt
-//Use this code if you want to.*/ //                                                              
-//confirm("This jQuery code is supposed to fade the div (the blue box) but it isn't working. I tried it on Codecademy and it worked. Scroll down to see the code from script.js.");	
-/*var user = prompt("Hello, I would like to meet you. What is your name?").toUpperCase();			
-																									
-	switch(user) {																					
-		case'AMIT': 																				
-			confirm("Hey, that's me!?!?");															
-		break;																						
-		case'NIKHIL': 																				
-			confirm("Congradulations");																
-		break;																					
-		case'AAI': 																				 
-			confirm("I hope you made some Varan Bhat.");										
-		break;																					 
-		case'BABA': 																			  
-			confirm("Enjoy some more Wada Pav");												   
-		break;
-		case'TAEWOO':
-			confirm("Hello Trumpet Buddy");
-		break;
-		case'ROY':
-			confirm("Hi");
-		break;
-		case'SANDEEP KAKA':
-			confirm("I can't wait t'ill I visit the windy city!");                                            
-		break;	
-		case'':
-			confirm("You didn't put a name. Restart the web and write one.");
-		break;
-		default:
-			confirm("I do not understand your choice. Please restart the web and start again.");
-}*/
+//for loop example
+/* 
+	for (var counter = 1; counter < 11; counter++) {
+		console.log(counter);
+	} 
+*/
+
+//while loop example
+/*
+	var coinFace = Math.floor(Math.random() * 2);
+
+	while(coinFace === 0){
+		console.log("Heads! Flipping again...");
+		var coinFace = Math.floor(Math.random() * 2);
+	}
+	console.log("Tails! Done flipping.");
+*/
+//do/while loop
+/*
+	do {
+		console.log("I'm gonna stop looping 'cause my condition is " + loopCondition + "!");	
+	}
+	while (loopCondition);
+*/
+//Array example
+/*
+	var cities = ["Melbourne", "Amman", "Helsinki", "NYC", "Atlanta", "Chicago", "Washington D.C."];
+*/
+
