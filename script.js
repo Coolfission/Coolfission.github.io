4f2d89f6686fbb003ec55c2ee03d8c1e61d8ae56
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-12-21 12:52:46 -0500

    changing opacity: 0 to display: none
    script.js and style.css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
   var phone = function() {
     if(screen.width <= 800) {
       $("div").attr('data', 'blur');
-      $("#ask").animate({"opacity": "1"}, 500);
+      $("#ask").animate({"display": "auto"}, 500);
     }
     $(".fa-times-circle-o").click(function() {
-      $("#ask").animate({"opacity": "0"}, 500);
+      $("#ask").animate({"display": "none"}, 500);
     });
   };
   $(document).ready(phone);
