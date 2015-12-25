4eef912981501e3cfd5230332bed4ebf9cefe670
Amit Kulkarni <amitkulkarni181@gmail.com>
2015-12-24 23:04:01 -0500

    test2
    converter

--- junk.js
+++ junk.js
@@ -1,19 +1,19 @@
-// $('#mile').keyup(function() { 
+$('#mile').keyup(function() { 
   
-//     $("#yard").val(parseInt($("#mile").val(), 10) * 1760);
-//     $("#foot").val(parseInt($("#mile").val(), 10) * 5280);
-//     $("#inch").val(parseInt($("#mile").val(), 10) * 63360);
-//     $("#nautical_mile").val(parseInt($("#mile").val(), 10) * 0.868976);
-// });
-// $('#yard').keyup(function() {
-//      $("#mile").val(parseInt($("#yard").val(), 10) * 0.000568182);
-//     $("#foot").val(parseInt($("#yard").val(), 10) * 5280);
-//     $("#inch").val(parseInt($("#yard").val(), 10) * 63360);
-//     $("#nautical_mile").val(parseInt($("#yard").val(), 10) * 0.868976);
-// });
+    $("#yard").val(parseInt($("#mile").val(), 10) * 1760);
+    $("#foot").val(parseInt($("#mile").val(), 10) * 5280);
+    $("#inch").val(parseInt($("#mile").val(), 10) * 63360);
+    $("#nautical_mile").val(parseInt($("#mile").val(), 10) * 0.868976);
+});
+$('#yard').keyup(function() {
+     $("#mile").val(parseInt($("#yard").val(), 10) * 0.000568182);
+    $("#foot").val(parseInt($("#yard").val(), 10) * 5280);
+    $("#inch").val(parseInt($("#yard").val(), 10) * 63360);
+    $("#nautical_mile").val(parseInt($("#yard").val(), 10) * 0.868976);
+});
 
-// $("input").val("0");
+$("input").val("0");
 
-// $("#reset").click(function() {
-//   $("input").val("0");
-// }); 
+$("#reset").click(function() {
+  $("input").val("0");
+}); 
