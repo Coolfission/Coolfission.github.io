d4ec9b345bcaaa1158d79ec89687f7868749e8e5
Amit Kulkarni <amitkulkarni181@gmail.com>
2016-06-30 12:06:30 -0400

    adding working # to main home page

--- c++/index.cpp
+++ c++/index.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-int main ()
-{
-  std::cout << "hi" << std::endl;
-  return 0;
+int main () {
+	int x;
+	std::cout << "Enter a number?";
+  	std::cin >> x;
+  	std::cout << "Your number is: " << x;
+ 	return 0;
 }
+// struct num {
+//     int n;
+//     Foo() {
+//        std::cout << "Enter n: ";
+//        std::cin >> n;   
+//     }
+// };
+// Foo num;
+// int main()
+// {
+//     std::cout << "The number is " << num.n << '\n';
+// }
--- script.js
+++ script.js
@@ -7,22 +7,34 @@ function open() {
       $("body *").not("#about *").css({"color": "#000"});
       $("#about").css({"color": "#fff"});
 }
-  $("a[style='cursor: pointer']").click(function() {
-    open();
-  });
+    $("a[style='cursor: pointer']").click(function() {
+      open();
+    });
+
+    var url = window.location.href;
+    var lastPart = url.substr(url.lastIndexOf('#') + 1);
+    if (lastPart === "about") {
+       open();
+    }
+    else if(lastPart === "") {
+      close();
+    }
 
 function close() {
   $("#about").attr("id", "about-closed");
       $("body").css({"background": "#ddd"});
       $("body *").css({"color": ""});
 }
-  shortcut.add("Esc", function() {
-    close();
-  });
-  $("i[class='fa fa-times-circle-o']").click(function() {
-    close();
-  });
+    shortcut.add("Esc", function() {
+      close();
+      location.hash = "";
+    });
+
+    $("i[class='fa fa-times-circle-o']").click(function() {
+      close();
+    });
 };
+
 $(document).ready(about);
 
 var typed = function() {
@@ -58,156 +70,3 @@ var time = function() {
   changeTime.innerHTML = greeting;
 };
 $(document).ready(time);
-
-
-$(function() {
-
-  $("#context-menu__print").click(function() {
-    window.print();
-  });
-
-  function clickInsideElement( e, className ) {
-    var el = e.srcElement || e.target;
-    
-    if ( el.classList.contains(className) ) {
-      return el;
-    } else {
-      while ( el = el.parentNode ) {
-        if ( el.classList && el.classList.contains(className) ) {
-          return el;
-        }
-      }
-    }
-
-    return false;
-  }
-  function getPosition(e) {
-    var posx = 0;
-    var posy = 0;
-
-    if (!e) var e = window.event;
-    
-    if (e.pageX || e.pageY) {
-      posx = e.pageX;
-      posy = e.pageY;
-    } else if (e.clientX || e.clientY) {
-      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-
-    return {
-      x: posx,
-      y: posy
-    }
-  }
-  var contextMenuClassName = "context-menu";
-  var contextMenuItemClassName = "context-menu__item";
-  var contextMenuLinkClassName = "context-menu__link";
-  var contextMenuActive = "context-menu--active";
-
-  var taskItemClassName = "task";
-  var taskItemInContext;
-
-  var clickCoords;
-  var clickCoordsX;
-  var clickCoordsY;
-
-  var menu = document.querySelector("#context-menu");
-  var menuItems = menu.querySelectorAll(".context-menu__item");
-  var menuState = 0;
-  var menuWidth;
-  var menuHeight;
-  var menuPosition;
-  var menuPositionX;
-  var menuPositionY;
-
-  var windowWidth;
-  var windowHeight;
-  function init() {
-    contextListener();
-    clickListener();
-    keyupListener();
-    resizeListener();
-  }
-  function contextListener() {
-    document.addEventListener( "contextmenu", function(e) {
-      taskItemInContext = clickInsideElement( e, taskItemClassName );
-
-      if ( taskItemInContext ) {
-        e.preventDefault();
-        toggleMenuOn();
-        positionMenu(e);
-      } else {
-        taskItemInContext = null;
-        toggleMenuOff();
-      }
-    });
-  }
-  function clickListener() {
-    document.addEventListener( "click", function(e) {
-      var clickeElIsLink = clickInsideElement( e, contextMenuLinkClassName );
-
-      if ( clickeElIsLink ) {
-        e.preventDefault();
-        menuItemListener( clickeElIsLink );
-      } else {
-        var button = e.which || e.button;
-        if ( button === 1 ) {
-          toggleMenuOff();
-        }
-      }
-    });
-  }
-  function keyupListener() {
-    window.onkeyup = function(e) {
-      if ( e.keyCode === 27 ) {
-        toggleMenuOff();
-      }
-    }
-  }
-  function resizeListener() {
-    window.onresize = function(e) {
-      toggleMenuOff();
-    };
-  }
-  function toggleMenuOn() {
-    if ( menuState !== 1 ) {
-      menuState = 1;
-      menu.classList.add( contextMenuActive );
-    }
-  }
-  function toggleMenuOff() {
-    if ( menuState !== 0 ) {
-      menuState = 0;
-      menu.classList.remove( contextMenuActive );
-    }
-  }
-  function positionMenu(e) {
-    clickCoords = getPosition(e);
-    clickCoordsX = clickCoords.x;
-    clickCoordsY = clickCoords.y;
-
-    menuWidth = menu.offsetWidth + 4;
-    menuHeight = menu.offsetHeight + 4;
-
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
-
-    if ( (windowWidth - clickCoordsX) < menuWidth ) {
-      menu.style.left = windowWidth - menuWidth + "px";
-    } else {
-      menu.style.left = clickCoordsX + "px";
-    }
-
-    if ( (windowHeight - clickCoordsY) < menuHeight ) {
-      menu.style.top = windowHeight - menuHeight + "px";
-    } else {
-      menu.style.top = clickCoordsY + "px";
-    }
-  }
-  function menuItemListener( link ) {
-    console.log( "Task ID - " + taskItemInContext.getAttribute("data-id") + ", Task action - " + link.getAttribute("data-action"));
-    toggleMenuOff();
-  }
-  init();
-});
