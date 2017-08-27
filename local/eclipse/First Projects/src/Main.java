e91fc3accd09a61edc5e8c67e296e2a610d670a3
Amit Kulkarni <amitkulkarni181@gmail.com>
2017-08-27 14:12:01 -0400

    Reverted index.html title

---
+++ local/eclipse/First Projects/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args ) {
+		int i="Hello World";
+		System.out.println(i);
+	/*	for(int i=1; i>0; i++) {
+			System.out.println(i);
+		}
+	*/
+	}
+}
