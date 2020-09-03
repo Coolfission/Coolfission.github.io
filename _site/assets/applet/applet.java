9b97efb17d798b60efc7d825da8bd5b20bc8a206
Amit Kulkarni <amitkulkarni181@gmail.com>
2020-09-03 11:57:50 -0400

    Cleaning up

--- _site/assets/applet/applet.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-import java.applet.*;
-// A minimal AWT-based applet.
-public class applet extends Applet {
-  public void paint(Graphics g) {
-    g.drawString("Java makes applets easy.", 20, 20);
- }
-}
